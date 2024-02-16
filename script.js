ee58013a2e19de51614ef55124d086052a4c9e2e
HDCODE <119462812+hdcodetj@users.noreply.github.com>
2024-02-16 14:19:25 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function cripto() {
+  let text = document.getElementById("text").value.toLowerCase();
+  let 
+
+/* 
+<section id="section1">
+      <textarea id="text" cols="40" rows="50" placeholder="Digite seu texto . . . "></textarea>
+      <p id="info_text">Apenas letras minúsculas e sem acento.</p>
+      <div id="buttons">
+         <button type="button" id="button1Cripto">Criptografar</button>
+         <button type="button" id="button2Descripto">Descriptografar</button>
+      </div>
+    </section>
+    <section id="section2">
+      <div id="text_result"></div>
+      <h4 id="info_result_none">NENHUMA MENSAGEM ENCONTRADA</h4>
+      <p id="info_result_type">Digite um texto que você deseja criptografar ou descriptografar.</p>
+    </section>   
+*/
